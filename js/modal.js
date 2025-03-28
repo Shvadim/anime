9c90678a55b49cba0afe92a59cd2211a79892f61
Шишковский Вадим Станиславович <vadzim.shyshkouski@b-logic.by>
2025-03-28 17:18:25 +0300

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.search-model')
+const iconSearch = document.querySelector('.icon_search')
+const searchCloseSwitch = modal.querySelector('.search-close-switch')
+
+iconSearch.addEventListener('click', e => {
+  modal.style.display = 'block'
+})
+
+searchCloseSwitch.addEventListener('click', e => {
+  modal.style.display = 'none'
+})
+
+// получение компонента строки ввода поиска
+const searchInput = document.getElementById('search-input')
+// обработчик нажатий на клавишу
+searchInput.addEventListener('keyup', e => {
+  console.log('key :>> ', e.key)
+  console.log('searchInput :>> ', searchInput.value)
+})
